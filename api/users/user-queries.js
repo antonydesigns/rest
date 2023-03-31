ab4849494f591fcb0b45d73ae969b6690142fc71
gio <gio>
2023-03-31 17:23:14 +0800

    Add complete CRUD package

--- api/users/user-queries.js
+++ api/users/user-queries.js
@@ -1,8 +1,7 @@
 import { db } from "../../config/database.js";
 
-export const addUserQuery = (data, callBack) => {
-  const stmt = `INSERT INTO users(username, password) values(?, ?)`;
-  const entries = [data.username, data.password];
+export const fetchAllUsersQuery = (callBack) => {
+  const stmt = `SELECT username FROM users`;
   const process = (error, results, fields) => {
     if (error) {
       return callBack(error);
@@ -11,7 +10,58 @@ export const addUserQuery = (data, callBack) => {
     // if everything is OK, callback passes null error and positive results to the Controller
     return callBack(null, results);
   };
+  // GET request to the MySQL database
+  db.query(stmt, [], process);
+};
 
+export const fetchOneUserQuery = (data, callBack) => {
+  const stmt = `SELECT username FROM users WHERE id=?`;
+  const entries = [data.id];
+  const process = (error, results, fields) => {
+    if (error) {
+      return callBack(error);
+    }
+    return callBack(null, results);
+  };
+  // GET request to the MySQL database
+  db.query(stmt, entries, process);
+};
+
+export const patchOneUserQuery = (data, callBack) => {
+  const stmt = `UPDATE users SET username=?, password=? WHERE id=?`;
+  const entries = [data.username, data.password];
+  const process = (error, results, fields) => {
+    if (error) {
+      return callBack(error);
+    }
+    return callBack(null, results);
+  };
+  // PATCH request to the MySQL database
+  db.query(stmt, entries, process);
+};
+
+export const addUserQuery = (data, callBack) => {
+  const stmt = `INSERT INTO users(username, password) values(?, ?)`;
+  const entries = [data.username, data.password];
+  const process = (error, results, fields) => {
+    if (error) {
+      return callBack(error);
+    }
+    return callBack(null, results);
+  };
   // POST request to the MySQL database
+  db.query(stmt, entries, process);
+};
+
+export const deleteUserQuery = (data, callBack) => {
+  const stmt = `DELETE FROM users WHERE id=?`;
+  const entries = [data.id];
+  const process = (error, results, fields) => {
+    if (error) {
+      return callBack(error);
+    }
+    return callBack(null, results);
+  };
+  // GET request to the MySQL database
   db.query(stmt, entries, process);
 };
