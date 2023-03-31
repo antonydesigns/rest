2787a3f9a981bbcca953b4f3890465500eebadbb
gio <gio>
2023-03-31 15:34:21 +0800

    Set up database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { a } from "./config/database.js";
 
 const app = express();
 
@@ -13,4 +14,5 @@ app.get(endpoints.getAll, (req, res) => {
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+  console.log(a);
 });
---
+++ config/database.js
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { createPool } from "mysql";
+
+export const db = createPool({
+  port: process.env.dev_port,
+  host: process.env.dev_host,
+  user: process.env.dev_user,
+  password: process.env.dev_password || "",
+  database: process.env.dev_database,
+});
+
+export const a = process.env.dev_database;
