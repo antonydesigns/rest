ec3a81d57ca4acf5cd9a1e37ed0546b2dabd2ed8
gio <gio>
2023-03-31 18:25:12 +0800

    Add controller for full CRUD

--- api/users/user-controller.js
+++ api/users/user-controller.js
@@ -1,21 +1,63 @@
-import { addUserQuery } from "./user-queries.js";
+import {
+  fetchAllUsersQuery,
+  fetchOneUserQuery,
+  patchOneUserQuery,
+  addUserQuery,
+  deleteUserQuery,
+} from "./user-queries.js";
 import { genSaltSync, hashSync } from "bcrypt";
+
+const resMsg = {
+  dbConnectError: {
+    message: "Database connection error",
+  },
+  recordNotFound: {
+    message: "Record not found",
+  },
+};
+
+export const fetchAllUsers = (req, res) => {
+  fetchAllUsersQuery((err, results) => {
+    if (err) return res.json(resMsg.dbConnectError);
+    if (!results) return res.json(resMsg.recordNotFound);
+    return res.status(200).json({ data: results });
+  });
+};
+
+export const fetchOneUser = (req, res) => {
+  const id = req.params.id;
+  fetchOneUserQuery(id, (err, results) => {
+    if (err) return res.json(resMsg.dbConnectError);
+    if (!results) return res.json(resMsg.recordNotFound);
+    return res.status(200).json({ data: results });
+  });
+};
+
+export const patchOneUser = (req, res) => {
+  const body = req.body;
+  const salt = genSaltSync(10);
+  body.password = hashSync(body.password, salt);
+  patchOneUserQuery(body, (err, results) => {
+    if (err) return res.json(resMsg.dbConnectError);
+    // error message for failed to update / patch?
+    return res.status(200).json({ data: results });
+  });
+};
+
+export const deleteUser = (req, res) => {
+  const id = req.params.id;
+  deleteUserQuery(id, (err, results) => {
+    if (err) return res.json(resMsg.dbConnectError);
+    return res.status(200).json({ data: results });
+  });
+};
 
 export const addUser = (req, res) => {
   const body = req.body;
   const salt = genSaltSync(10);
   body.password = hashSync(body.password, salt);
   addUserQuery(body, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({
-        success: 0,
-        message: "Database connection error",
-      });
-    }
-    return res.status(200).json({
-      success: 1,
-      data: results,
-    });
+    if (err) return res.status(500).json(resMsg.dbConnectError);
+    return res.status(200).json({ data: results });
   });
 };
--- api/users/user-queries.js
+++ api/users/user-queries.js
@@ -3,10 +3,9 @@ import { db } from "../../config/database.js";
 export const fetchAllUsersQuery = (callBack) => {
   const stmt = `SELECT username FROM users`;
   const process = (error, results, fields) => {
-    if (error) {
-      return callBack(error);
-      // if database connection is bad, callback passes an error to the Controller
-    }
+    // if database connection is bad, callback passes an error to the Controller
+    if (error) return callBack(error);
+
     // if everything is OK, callback passes null error and positive results to the Controller
     return callBack(null, results);
   };
@@ -18,9 +17,8 @@ export const fetchOneUserQuery = (data, callBack) => {
   const stmt = `SELECT username FROM users WHERE id=?`;
   const entries = [data.id];
   const process = (error, results, fields) => {
-    if (error) {
-      return callBack(error);
-    }
+    if (error) return callBack(error);
+
     return callBack(null, results);
   };
   // GET request to the MySQL database
@@ -31,9 +29,7 @@ export const patchOneUserQuery = (data, callBack) => {
   const stmt = `UPDATE users SET username=?, password=? WHERE id=?`;
   const entries = [data.username, data.password];
   const process = (error, results, fields) => {
-    if (error) {
-      return callBack(error);
-    }
+    if (error) return callBack(error);
     return callBack(null, results);
   };
   // PATCH request to the MySQL database
@@ -44,9 +40,7 @@ export const addUserQuery = (data, callBack) => {
   const stmt = `INSERT INTO users(username, password) values(?, ?)`;
   const entries = [data.username, data.password];
   const process = (error, results, fields) => {
-    if (error) {
-      return callBack(error);
-    }
+    if (error) return callBack(error);
     return callBack(null, results);
   };
   // POST request to the MySQL database
@@ -57,9 +51,7 @@ export const deleteUserQuery = (data, callBack) => {
   const stmt = `DELETE FROM users WHERE id=?`;
   const entries = [data.id];
   const process = (error, results, fields) => {
-    if (error) {
-      return callBack(error);
-    }
+    if (error) return callBack(error);
     return callBack(null, results);
   };
   // GET request to the MySQL database
