f17548aa134da7e630b1aa3f56b1939be0f103e1
gio <gio>
2023-03-31 15:14:56 +0800

    Endpoint connected

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 import express from "express";
 
 const app = express();
+
+const endpoints = {
+  getAll: "/api/",
+};
+
+app.get(endpoints.getAll, (req, res) => {
+  const result = { success: 1, message: "hello world" };
+  res.json(result);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
